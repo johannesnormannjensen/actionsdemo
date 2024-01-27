58f7a8ebd92405536c03cbf5ac55e670b8fa6070
Flemming Bregnvig <flemming@bregnvig.dk>
2024-01-27 12:52:14 +0100

    rename to ngneers

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterParamsService, UserService } from '@nestext/ui/service';
+import { RouterParamsService, UserService } from '@ngneers/ui/service';
 
 @Component({
-  selector: 'nestext-root',
-  template: `
+    selector: 'ngneers-root',
+    template: `
     <common-toast aria-live="polite" aria-atomic="true"></common-toast>
     <common-nav [user]="user$ | async" (logout)="logout()"></common-nav>
     <router-outlet></router-outlet>
   `,
 })
 export class AppComponent {
-  user$ = this.userService.user$;
+    user$ = this.userService.user$;
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  constructor(private userService: UserService, routerParams: RouterParamsService) {
-  }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    constructor(private userService: UserService, routerParams: RouterParamsService) {
+    }
 
-  logout() {
-    this.userService.logout();
-  }
+    logout() {
+        this.userService.logout();
+    }
 }
--- apps/ui/src/app/app.module.ts
+++ apps/ui/src/app/app.module.ts
@@ -7,58 +7,57 @@ import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angul
 import { connectFunctionsEmulator, getFunctions, provideFunctions } from '@angular/fire/functions';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { NavComponent } from '@nestext/ui/common';
-import { ToastComponent } from '@nestext/ui/common';
-import { environment } from '@nestext/ui/environment';
+import { NavComponent, ToastComponent } from '@ngneers/ui/common';
+import { environment } from '@ngneers/ui/environment';
+import { initializers, ngneersTranslateModule } from '@ngneers/ui/service';
 import { LumberjackLevel, provideLumberjack } from '@ngworker/lumberjack';
 import { provideLumberjackConsoleDriver } from '@ngworker/lumberjack/console-driver';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { initializers, NestextTranslateModule } from '@nestext/ui/service';
 
 @NgModule({
-  imports: [
-    NavComponent,
-    ToastComponent,
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    HttpClientModule,
-    provideFirebaseApp(() => {
-      const app = initializeApp(environment.firebaseConfig);
-      isDevMode() && console.log('Initializing Firebase app', environment.firebaseConfig);
-      if (environment.useEmulator) {
-        console.warn('Using auth emulator');
-        connectAuthEmulator(getAuth(), 'http://localhost:9099');
-      }
-      return app;
-    }),
-    provideFirestore(() => {
-      const db = getFirestore();
-      if (environment.useEmulator) {
-        connectFirestoreEmulator(db, 'localhost', 8080);
-        console.warn('Using firestore emulator');
-      }
-      return db;
-    }),
-    provideFunctions(() => {
-      const functions = getFunctions(undefined, 'europe-west1');
-      if (environment.useEmulator) {
-        connectFunctionsEmulator(functions, 'localhost', 5001);
-        console.warn('Using functions emulator');
-      }
-      return functions;
-    }),
-    NestextTranslateModule,
-  ],
-  declarations: [AppComponent],
-  providers: [
-    ...initializers,
-    provideLumberjack(),
-    provideLumberjackConsoleDriver({
-      levels: environment.production ? [LumberjackLevel.Error] : [LumberjackLevel.Debug, LumberjackLevel.Info, LumberjackLevel.Warning, LumberjackLevel.Error],
-    }),
-  ],
-  bootstrap: [AppComponent],
+    imports: [
+        NavComponent,
+        ToastComponent,
+        BrowserModule,
+        RouterModule.forRoot(appRoutes),
+        HttpClientModule,
+        provideFirebaseApp(() => {
+            const app = initializeApp(environment.firebaseConfig);
+            isDevMode() && console.log('Initializing Firebase app', environment.firebaseConfig);
+            if (environment.useEmulator) {
+                console.warn('Using auth emulator');
+                connectAuthEmulator(getAuth(), 'http://localhost:9099');
+            }
+            return app;
+        }),
+        provideFirestore(() => {
+            const db = getFirestore();
+            if (environment.useEmulator) {
+                connectFirestoreEmulator(db, 'localhost', 8080);
+                console.warn('Using firestore emulator');
+            }
+            return db;
+        }),
+        provideFunctions(() => {
+            const functions = getFunctions(undefined, 'europe-west1');
+            if (environment.useEmulator) {
+                connectFunctionsEmulator(functions, 'localhost', 5001);
+                console.warn('Using functions emulator');
+            }
+            return functions;
+        }),
+        ngneersTranslateModule,
+    ],
+    declarations: [AppComponent],
+    providers: [
+        ...initializers,
+        provideLumberjack(),
+        provideLumberjackConsoleDriver({
+            levels: environment.production ? [LumberjackLevel.Error] : [LumberjackLevel.Debug, LumberjackLevel.Info, LumberjackLevel.Warning, LumberjackLevel.Error],
+        }),
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
